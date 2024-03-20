459b5c13cda8bc2d37d9ba8f6124766f6687f80c
zigzaga00 <161963582+zigzaga00@users.noreply.github.com>
2024-03-20 21:04:34 +0000

    ball bounce methods added

--- ball.py
+++ ball.py
@@ -5,9 +5,18 @@ class Ball(Turtle):
         super().__init__()
         self.color("white")
         self.shape("square")
+        self.x_move = 5
+        self.y_move = 5
         self.penup()
-        
+
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def bounce_y(self):
+        self.y_move *= -1
+
     def move_ball(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+    
